867c0ffb238936fd0f149e50db531fd0cdf8e7e6
Zhixin Wen <zwen@roblox.com>
2025-07-13 22:26:33 +0000

    Fix WAIT

--- src/commands/cmd_replication.cc
+++ src/commands/cmd_replication.cc
@@ -347,8 +347,8 @@ class CommandDBName : public Commander {
 class CommandWait : public Commander {
  public:
   Status Parse(const std::vector<std::string> &args) override {
-    auto num_replicas_result = ParseInt<int>(args[1], {1, UINT64_MAX}, 10);
-    if (!num_replicas_result) {
+    auto num_replicas_result = ParseInt<int>(args[1], 10);
+    if (!num_replicas_result || *num_replicas_result <= 0) {
       return {Status::RedisParseErr, "numreplicas should be a positive integer"};
     }
 
