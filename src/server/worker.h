2be8d8c4a3774647fae576f2a4a30943b6010bde
Zhixin Wen <zwen@roblox.com>
2025-08-14 23:43:35 +0000

    remove EnableReadEvent

--- src/server/worker.h
+++ src/server/worker.h
@@ -59,7 +59,6 @@ class Worker : EventCallbackBase<Worker>, EvconnlistenerBase<Worker> {
   void FreeConnectionByID(int fd, uint64_t id);
   Status AddConnection(redis::Connection *c);
   Status EnableWriteEvent(int fd);
-  Status EnableReadEvent(int fd);
   Status Reply(int fd, const std::string &reply);
   void BecomeMonitorConn(redis::Connection *conn);
   void QuitMonitorConn(redis::Connection *conn);
